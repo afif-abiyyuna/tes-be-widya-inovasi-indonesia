d0e424129f8c28bc5fa7810c4d3e36231559e5f0
afif12-droid <afifabiyyuna1210@gmail.com>
2021-02-27 03:47:44 +0700

    commit kedua

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
 const errorhandler = require("../middlewares/errorHandler");
 
-route.use("/auth", authRoute);
-route.use("/board", userRoute);
+route.use("auth", authRoute);
+route.use("board", userRoute);
 
 route.use(errorhandler);
 
